a6222ec4f03fb90bf9d73a9602191f714068ed7f
Tony Xie <tonyxie@Tonys-MacBook-Pro.local>
2018-11-15 15:27:06 -0800

    set up routes and controller

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,7 @@
 const controller = require('../controller/models');
 
 module.exports = app => {
+    app.get('/login', controller.login);
+    app.get('/callback', controller.callback);
     app.all('*', controller.angular);
 }
