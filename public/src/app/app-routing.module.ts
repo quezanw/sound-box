52947f6a47e2ae88106c311c2560486103414557
Tony Xie <tonyxie@Tonys-MacBook-Pro.local>
2018-11-15 19:01:35 -0800

    routing for rooms component

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RoomsComponent } from './rooms/rooms.component';
 
 const routes: Routes = [
-    {path: '', component: LoginComponent}
+    {path: '', component: LoginComponent},
+    {path: 'rooms', component: RoomsComponent}
 ];
 
 @NgModule({
