7e315126c99e080be52e669dd3ea786e911ba94b
Tony Xie <tonyxie@Tonys-MacBook-Pro.local>
2018-11-17 11:32:10 -0800

    searching for song returns top 5 results

--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -5,14 +5,23 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class HttpService {
+  refresh_token: any;
 
   constructor(private _http: HttpClient) { }
 
   login() {
-    return this._http.get('/login', {withCredentials: true});
+    return this._http.get('/login');
   }
 
-  getSong(name) {
-    return this._http.get('/get_song/' + name, {withCredentials: true});
+  getSong(name: string, refresh_token: string) {
+    return this._http.get('/get_song/' + name + '/' + refresh_token);
+  }
+
+  setRefreshToken(data: string) {
+    this.refresh_token = data;
+  }
+
+  getRefreshToken() {
+    return this.refresh_token;
   }
 }
