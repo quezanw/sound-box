75a30ec9537bde16d2bbae3ad8891c7ce2fe05ac
Tony Xie <tonyxie@Tonys-MacBook-Pro.local>
2018-11-17 10:22:06 -0800

    passed refresh token through to rooms page through the url

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpService } from './http.service';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +21,7 @@ import { RoomComponent } from './room/room.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule
 
   ],
   providers: [HttpService],
