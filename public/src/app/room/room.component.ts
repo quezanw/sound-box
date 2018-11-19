817018ff27e4a2c2fc67bddeee0d4d54b7b3127e
Tony Xie <tonyxie@Tonys-MacBook-Pro.local>
2018-11-18 17:58:21 -0800

    current song information displays while playing

--- public/src/app/room/room.component.ts
+++ public/src/app/room/room.component.ts
@@ -11,6 +11,9 @@ export class RoomComponent implements OnInit {
   searchSong: string;
   searchResults: any[];
   refresh_token: string;
+  currentSong: any;
+  upvoted: boolean;
+
   constructor(private _httpService: HttpService) { }
 
   ngOnInit() {
@@ -19,7 +22,9 @@ export class RoomComponent implements OnInit {
     this.searchResults = [];
     // this.refresh_token = this._httpService.getRefreshToken();
     this.refresh_token = "AQAb-V7CTJiFtkEx3lqaIsv5_8y__H5r85qHCQRhz4O0i1VZJMMwMnL-htnOxbzYE_VxgKzh44tc964xEVBi7pUejLOqVeqvY-uiUQzLQ9XJn7G2goUeyLX22zJcFxNkVqo-fA";
-    console.log(this.refresh_token)
+    console.log(this.refresh_token);
+    this.currentSong = null;
+    this.upvoted = true;
   }
 
   findSong(): void {
@@ -35,14 +40,25 @@ export class RoomComponent implements OnInit {
     console.log(song);
     this.searchResults = [];
     this.queue.push({info: song, upvotes: 0});
-    let observable = this._httpService.playSong({song_uri: song.uri, refresh_token: this.refresh_token});
-    observable.subscribe(data => console.log(data));
+    if (!this.currentSong) {
+      this.playSong();
+    } 
   }
 
   upvote(song: any): void {
     song.upvotes++;
+    this.upvoted = false;
     this.queue.sort(function(a, b) {
       return b.upvotes - a.upvotes;
+    });
+  }
+
+  playSong(): void {
+    this.currentSong = this.queue[0];
+    this.queue.shift();
+    let observable = this._httpService.playSong({song_uri: this.currentSong.info.uri, refresh_token: this.refresh_token});
+    observable.subscribe(data => {
+      setTimeout(() => this.playSong(), this.currentSong.info.duration_ms + 2000);
     });
   }
 
